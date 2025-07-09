99abcaaf1337b5e0718f8cc13c31ed8ede6980bc
hafsahtariqali <alihafsatariq@gmail.com>
2025-07-09 15:14:59 +0500

    fix: added missing path module

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const User = require('./models/users');
+const path = require('path');
 
 //initializing app
 const app = express();
